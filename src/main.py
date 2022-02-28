db0dd8f42e3ecc1ae5f8a45cf2766b7327d893f0
Neelay Joglekar <njogleka@ucsd.edu>
2022-02-28 15:22:53 -0800

    Pixel tip locations

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+"""
+Tip locations:
+    Left- x=336, y=258 (bottom rightmost pixel)
+    Right- x=313, y=258 (bottom rightmost pixel)
+"""
+if __name__ == "__main__":
+    " For finding image pixels "
+    # img = mpimg.imread("/Users/neelay/ARClabXtra/Sarah_imgs/thread_1_right_rembg.png")
+    # plt.imshow(img)
+    # plt.show()
