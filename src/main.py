3530eda06fae4d8f25e2ed03e3f8eb159f5abbb8
Neelay Joglekar <njogleka@ucsd.edu>
2022-03-01 21:31:56 -0800

    out-of-range count seems promising

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ Tip locations:
 """
 if __name__ == "__main__":
     """ #For finding image pixels
-    img = mpimg.imread("/Users/neelay/ARClabXtra/Sarah_imgs/thread_1_right_rembg.png")
+    img = mpimg.imread("/Users/neelay/ARClabXtra/Sarah_imgs/thread_1_left_rembg.png")
     plt.imshow(img)
     plt.show()
     """
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     img_r = cv2.imread(img_dir + "thread_1_right_rembg.png")
     img_r = cv2.cvtColor(img_r, cv2.COLOR_BGR2GRAY)
     thresh = 250
-    upsilon = 3
+    upsilon = 5
     roi = []
     for i in range(-upsilon, upsilon+1):
         for j in range(-upsilon, upsilon+1):
@@ -35,13 +35,41 @@ if __name__ == "__main__":
     active_l = []
     for i, j in roi:
         node_l = (i+curr_V_l[0], j+curr_V_l[1])
-        if (node_l not in segmented_l) and img_l[node_l] < thresh:
+        if ((node_l[0] < img_l.shape[0]) and (node_l[1] < img_l.shape[1]) and
+            (node_l not in segmented_l) and (img_l[node_l] <= thresh)
+            ):
             active_l.append(node_l)
     
     # Order pixels and stereo match
     while len(active_l):
         # calculate min cost active node
-        min_cost = np.Inf
-        for pr_l, pc_l in active_l:
-            # out of range numbers
-            slope = (pr_l - curr_V_l[0]) / (pc_l - curr_V_l[1])
+        min_cost_l = np.Inf
+        for prow_l, pcol_l in active_l:
+            # calculate out of range number
+            o_pixels = set()
+            delta_row_l = prow_l - curr_V_l[0]
+            delta_col_l = pcol_l - curr_V_l[1]
+            for i in range(abs(delta_row_l)):
+                sign = 1 if delta_row_l > 0 else -1
+                drow_l = (0.5 + i) * sign
+                dcol_l = delta_col_l/delta_row_l * drow_l
+                pixel1_l = (curr_V_l[0] + i*sign, round(curr_V_l[1] + dcol_l))
+                pixel2_l = (pixel1_l[0] + 1*sign, pixel1_l[1])
+                if img_l[pixel1_l] > thresh:
+                    o_pixels.add(pixel1_l)
+                if img_l[pixel2_l] > thresh:
+                    o_pixels.add(pixel2_l)
+
+            for i in range(abs(delta_col_l)):
+                sign = 1 if delta_col_l > 0 else -1
+                dcol_l = (0.5 + i) * sign
+                drow_l = delta_row_l/delta_col_l * dcol_l
+                pixel1_l = (round(curr_V_l[0] + drow_l), curr_V_l[1] + i*sign)
+                pixel2_l = (pixel1_l[0], pixel1_l[1] + 1*sign)
+                if img_l[pixel1_l] > thresh:
+                    o_pixels.add(pixel1_l)
+                if img_l[pixel2_l] > thresh:
+                    o_pixels.add(pixel2_l)
+            
+            O_num = len(o_pixels)
+        break
