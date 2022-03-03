08e8aa67c7699ab4377502d8259bac877edbc037
Neelay Joglekar <njogleka@ucsd.edu>
2022-03-03 14:46:18 -0800

    cost calculation runs

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 import cv2
+import math
 
 """
 Tip locations:
@@ -9,11 +10,11 @@ Tip locations:
     Right- x=313, y=258 (bottom rightmost pixel)
 """
 if __name__ == "__main__":
-    #""" #For finding image pixels
+    """ #For finding image pixels
     img = mpimg.imread("/Users/neelay/ARClabXtra/Sarah_imgs/thread_1_left_rembg.png")
     plt.imshow(img)
     plt.show()
-    #"""
+    """
     # Set up image and useful constants
     img_dir = "/Users/neelay/ARClabXtra/Sarah_imgs/"
     img_l = cv2.imread(img_dir + "thread_1_left_rembg.png")
@@ -41,12 +42,30 @@ if __name__ == "__main__":
             active_l.append(node_l)
     
     # Order pixels and stereo match
+    e_1 = 3
+    e_2 = 0.3
+    e_3 = 0.05
+    mu = 1
+    tau_O = 2.6 * upsilon
+    tau_V = 2*math.pi/3
     while len(active_l):
         # calculate min cost active node
         min_cost_l = np.Inf
+        min_node_l = None
         for prow_l, pcol_l in active_l:
+            # Calculate triangle area terms
+            to_active_l = np.array([prow_l, pcol_l]) - np.array([curr_V_l[0], curr_V_l[1]])
+            to_prev_l = np.array([curr_V_l[0], curr_V_l[1]]) - np.array([par_V_l[0], par_V_l[1]])
+            angle_l = np.arccos(
+                np.dot(to_active_l, to_prev_l) /
+                (np.linalg.norm(to_active_l) * np.linalg.norm(to_prev_l))
+            )
+            # Compare with terminating threshold
+            if (angle_l >= tau_V):
+                continue
+            
             # calculate out of range number
-            o_pixels = set()
+            o_pixels_l = set()
             delta_row_l = prow_l - curr_V_l[0]
             delta_col_l = pcol_l - curr_V_l[1]
             for i in range(abs(delta_row_l)):
@@ -56,9 +75,9 @@ if __name__ == "__main__":
                 pixel1_l = (curr_V_l[0] + i*sign, round(curr_V_l[1] + dcol_l))
                 pixel2_l = (pixel1_l[0] + 1*sign, pixel1_l[1])
                 if img_l[pixel1_l] > thresh:
-                    o_pixels.add(pixel1_l)
+                    o_pixels_l.add(pixel1_l)
                 if img_l[pixel2_l] > thresh:
-                    o_pixels.add(pixel2_l)
+                    o_pixels_l.add(pixel2_l)
 
             for i in range(abs(delta_col_l)):
                 sign = 1 if delta_col_l > 0 else -1
@@ -67,17 +86,23 @@ if __name__ == "__main__":
                 pixel1_l = (round(curr_V_l[0] + drow_l), curr_V_l[1] + i*sign)
                 pixel2_l = (pixel1_l[0], pixel1_l[1] + 1*sign)
                 if img_l[pixel1_l] > thresh:
-                    o_pixels.add(pixel1_l)
+                    o_pixels_l.add(pixel1_l)
                 if img_l[pixel2_l] > thresh:
-                    o_pixels.add(pixel2_l)
+                    o_pixels_l.add(pixel2_l)
             
-            O_num = len(o_pixels)
-            
-            # Calculate triangle area terms
-            to_active = np.array([prow_l, pcol_l]) - np.array([curr_V_l[0], curr_V_l[1]])
-            to_prev = np.array([curr_V_l[0], curr_V_l[1]]) - np.array([par_V_l[0], par_V_l[1]])
-            angle = np.arccos(
-                np.dot(to_active, to_prev) /
-                (np.linalg.norm(to_active) * np.linalg.norm(to_prev))
+            O_num_l = len(o_pixels_l)
+            # Compare with terminating threshold
+            if (O_num_l >= tau_O):
+                continue
+
+            # Calculate node cost and compare to min cost
+            cost_l = (
+                math.log(e_1*O_num_l + 1) + 
+                e_2 * np.linalg.norm(to_active_l) *
+                np.exp(e_3 * np.sin(angle_l/2))
             )
+            print(prow_l, pcol_l, cost_l)
+            if (cost_l < min_cost_l):
+                min_cost_l = cost_l
+                min_node_l = (prow_l, pcol_l)
         break
