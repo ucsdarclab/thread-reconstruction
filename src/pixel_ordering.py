921d4ebf199edf299d91d5a8df6726bc6080e9fe
Neelay Joglekar <njogleka@ucsd.edu>
2022-04-05 20:15:16 -0700

    Orders both left and right images

--- src/pixel_ordering.py
+++ src/pixel_ordering.py
@@ -8,11 +8,11 @@ np.seterr(all="raise")
 """
 Tip locations:
     Left- x=337, y=259 (bottom rightmost pixel)
-    Right- x=313, y=258 (bottom rightmost pixel)
+    Right- x=314, y=259 (bottom rightmost pixel)
 """
 def order_pixels():
     #""" #For finding image pixels
-    img = mpimg.imread("/Users/neelay/ARClabXtra/Sarah_imgs/thread_1_left_rembg.png")
+    # img = mpimg.imread("/Users/neelay/ARClabXtra/Sarah_imgs/thread_1_right_rembg.png")
     # TODO Uncomment
     # plt.imshow(img)
     # plt.show()
@@ -22,9 +22,11 @@ def order_pixels():
     img_dir = "/Users/neelay/ARClabXtra/Sarah_imgs/"
     img_l = cv2.imread(img_dir + "thread_1_left_rembg.png")
     img_l = cv2.cvtColor(img_l, cv2.COLOR_BGR2GRAY)
+    img_l_init = img_l.copy()
     img_r = cv2.imread(img_dir + "thread_1_right_rembg.png")
     img_r = cv2.cvtColor(img_r, cv2.COLOR_BGR2GRAY)
-    thresh = 205
+    img_r_init = img_r.copy()
+    thresh = 210
     upsilon = 10
     roi = []
     for i in range(-upsilon, upsilon+1):
@@ -37,28 +39,44 @@ def order_pixels():
     #img_l_dilated = cv2.dilate(np.where(img_l <= thresh, 1, 0).astype("uint8"), dilate_kernel, iterations=2)
     img_l *= cv2.erode(np.where(img_l <= thresh, 1, 0).astype("uint8"), erode_kernel, iterations=1)
     img_l =np.where(img_l == 0, 255, img_l)
+    img_r *= cv2.erode(np.where(img_r <= thresh, 1, 0).astype("uint8"), erode_kernel, iterations=1)
+    img_r =np.where(img_r == 0, 255, img_r)
     # cv2.imshow("img", img_l)
     # cv2.waitKey(5000)
     # plt.imshow(img_l)
     # plt.show()
     # exit(0)
     
+    
     curr_V_l = (259, 336)
     par_V_l = (259, 337)
+    curr_V_r = (259, 313)
+    par_V_r = (259, 314)
     curve_set_l = {curr_V_l, par_V_l}
+    curve_set_r = {curr_V_r, par_V_r}
     curve_l = np.array([
         [par_V_l[1], curr_V_l[1]],
         [par_V_l[0], curr_V_l[0]]
     ])
+    curve_r = np.array([
+        [par_V_r[1], curr_V_r[1]],
+        [par_V_r[0], curr_V_r[0]]
+    ])
 
     # Pixel ordering setup
     active_l = []
+    active_r = []
     for i, j in roi:
         node_l = (i+curr_V_l[0], j+curr_V_l[1])
+        node_r = (i+curr_V_r[0], j+curr_V_r[1])
         if ((node_l[0] < img_l.shape[0]) and (node_l[1] < img_l.shape[1]) and
             (node_l not in curve_set_l) and (img_l[node_l] <= thresh)
             ):
             active_l.append(node_l)
+        if ((node_r[0] < img_r.shape[0]) and (node_r[1] < img_r.shape[1]) and
+            (node_r not in curve_set_r) and (img_r[node_r] <= thresh)
+            ):
+            active_r.append(node_r)
     
     # Order pixels and stereo match
     e_1 = 1
@@ -153,12 +171,104 @@ def order_pixels():
                 (node_l not in curve_set_l) and (img_l[node_l] <= thresh)
                 ):
                 active_l.append(node_l)
+
+    while len(active_r):
+        # calculate min cost active node
+        min_cost_r = np.Inf
+        min_nodes_r = []
+        for prow_r, pcol_r in active_r:
+            # Calculate triangle area terms
+            to_active_r = np.array([prow_r, pcol_r]) - np.array([curr_V_r[0], curr_V_r[1]])
+            to_prev_r = np.array([curr_V_r[0], curr_V_r[1]]) - np.array([par_V_r[0], par_V_r[1]])
+            angle_r = np.arccos(np.clip(
+                np.dot(to_active_r, to_prev_r) /
+                (np.linalg.norm(to_active_r) * np.linalg.norm(to_prev_r)),
+                -1,
+                1
+            ))
+            # Compare with terminating threshold
+            if (angle_r >= tau_V):
+                continue
+            
+            # calculate out of range number
+            o_pixels_r = set()
+            delta_row_r = prow_r - curr_V_r[0]
+            delta_col_r = pcol_r - curr_V_r[1]
+            for i in range(abs(delta_row_r)):
+                sign = 1 if delta_row_r > 0 else -1
+                drow_r = (0.5 + i) * sign
+                dcol_r = delta_col_r/delta_row_r * drow_r
+                pixel1_r = (curr_V_r[0] + i*sign, round(curr_V_r[1] + dcol_r))
+                pixel2_r = (pixel1_r[0] + 1*sign, pixel1_r[1])
+                if img_r[pixel1_r] > thresh:
+                    o_pixels_r.add(pixel1_r)
+                if img_r[pixel2_r] > thresh:
+                    o_pixels_r.add(pixel2_r)
+
+            for i in range(abs(delta_col_r)):
+                sign = 1 if delta_col_r > 0 else -1
+                dcol_r = (0.5 + i) * sign
+                drow_r = delta_row_r/delta_col_r * dcol_r
+                pixel1_r = (round(curr_V_r[0] + drow_r), curr_V_r[1] + i*sign)
+                pixel2_r = (pixel1_r[0], pixel1_r[1] + 1*sign)
+                if img_r[pixel1_r] > thresh:
+                    o_pixels_r.add(pixel1_r)
+                if img_r[pixel2_r] > thresh:
+                    o_pixels_r.add(pixel2_r)
+            
+            O_num_r = len(o_pixels_r)
+            # Compare with terminating threshold
+            if (O_num_r >= tau_O):
+                continue
+
+            # Calculate node cost and compare to min cost
+            cost_r = (
+                math.log(e_1*O_num_r + 1) + 
+                e_2 * np.linalg.norm(to_active_r) +
+                np.exp(e_3 * np.sin(angle_r/2))
+            )
+            if (cost_r < min_cost_r):
+                min_cost_r = cost_r
+                min_nodes_r = [(prow_r, pcol_r)]
+            elif (cost_r == min_cost_r):
+                min_nodes_r.append((prow_r, pcol_r))
+        
+        # Terminate if conditions all tripped
+        if (len(min_nodes_r) == 0):
+            break
+        # Add selected node to curve
+        min_node_r = min_nodes_r[random.randrange(0, len(min_nodes_r))]
+        #for min_node_r in min_nodes_r:
+        curve_set_r.add(min_node_r)
+        curve_r = np.concatenate(
+            (
+                curve_r,
+                np.array([[min_node_r[1]], [min_node_r[0]]])
+            ),
+            axis=1
+        )
+
+        # Update active nodes and curve nodes
+        par_V_r = curr_V_r
+        curr_V_r = min_node_r #min_nodes_r[random.randrange(0, len(min_nodes_r))]
+        active_r = []
+        for i, j in roi:
+            node_r = (i+curr_V_r[0], j+curr_V_r[1])
+            if ((node_r[0] < img_r.shape[0]) and (node_r[1] < img_r.shape[1]) and
+                (node_r not in curve_set_r) and (img_r[node_r] <= thresh)
+                ):
+                active_r.append(node_r)
     
     curve_l = curve_l[:, :-25]
-    # plt.scatter(curve_l[0], curve_l[1], c=np.linspace(0, curve_l.shape[1]-1, curve_l.shape[1]), cmap="hot")
-    # plt.show()
+    curve_r = curve_r[:, :-4]
+    plt.imshow(img_l_init, cmap="gray")
+    plt.scatter(curve_l[0], curve_l[1], c=np.linspace(0, curve_l.shape[1]-1, curve_l.shape[1]), cmap="hot")
+    plt.show()
+    plt.imshow(img_r_init, cmap="gray")
+    plt.scatter(curve_r[0], curve_r[1], c=np.linspace(0, curve_r.shape[1]-1, curve_r.shape[1]), cmap="hot")
+    plt.show()
 
-    return curve_l
+    return curve_l, curve_r
 
 if __name__ == "__main__":
     order_pixels()
