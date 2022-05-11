a3427707d5d65fe364675e6227d0be2af068c21c
Neelay Joglekar <njogleka@ucsd.edu>
2022-05-10 23:52:00 -0700

    Testing 3D DSA, checkpoint

--- src/curve_fit.py
+++ src/curve_fit.py
@@ -187,14 +187,30 @@ def fit_2D_curves():
     return final_curve[0].detach(), final_curve[1].detach()
 
 def fit_3D_curve():
+    img_dir = "/Users/neelay/ARClabXtra/Sarah_imgs/"
+    img_l = cv2.imread(img_dir + "thread_1_left_rembg.png")
+    img_l = cv2.cvtColor(img_l, cv2.COLOR_BGR2GRAY)
+    img_r = cv2.imread(img_dir + "thread_1_right_rembg.png")
+    img_r = cv2.cvtColor(img_r, cv2.COLOR_BGR2GRAY)
+
     P1, P2, points_3D = stereo_match()
+    P1 = torch.tensor(P1, dtype=torch.float32)
+    P2 = torch.tensor(P2, dtype=torch.float32)
     # in x, y, z order
     points_3D = torch.tensor(points_3D).permute(1, 0)
 
     # Initialize useful evaluation constants
     init_control_pts = None
+    init_ctrl_l = None
+    init_ctrl_r = None
+    final_ctrl_l = None
+    final_ctrl_r = None
     initial_curve = None
+    init_curve_l = None
+    init_curve_r = None
     final_curve = None
+    final_curve_l = None
+    final_curve_r = None
 
     eval_3D = 600
     eval_2D = 150
@@ -237,6 +253,16 @@ def fit_3D_curve():
     )
     if TESTING:
         init_control_pts = control_pts.clone()
+        aug_ctrl_pts = torch.cat(
+            (init_control_pts, torch.ones((1, init_control_pts.size(1)))), dim=0
+        ).permute(1, 0).unsqueeze(-1)
+        #x, y, z
+        init_ctrl_l = torch.matmul(P1, aug_ctrl_pts)
+        for i in range(init_ctrl_l.size(0)):
+            init_ctrl_l[i] /= init_ctrl_l[i, 2, 0].clone()
+        init_ctrl_r = torch.matmul(P2, aug_ctrl_pts)
+        for i in range(init_ctrl_r.size(0)):
+            init_ctrl_r[i] /= init_ctrl_r[i, 2, 0].clone()
     control_pts.requires_grad = True
 
     # Initialize ground truth
@@ -247,18 +273,19 @@ def fit_3D_curve():
     idx1 = [(i * ord1.size(1)) // eval_2D for i in range(eval_2D)]
     idx2 = [(i * ord2.size(1)) // eval_2D for i in range(eval_2D)]
 
+    #y, x
     target1 = torch.stack(
         (
             torch.tensor([ord1[1, i] for i in idx1]),
             torch.tensor([ord1[0, i] for i in idx1])
         )
-    )
+    ).permute(1, 0)
     target2 = torch.stack(
         (
             torch.tensor([ord2[1, i] for i in idx2]),
             torch.tensor([ord2[0, i] for i in idx2])
         )
-    )
+    ).permute(1, 0)
 
     # spacings = [
     #     target1[:, 1:] - target1[:, :-1],
@@ -272,13 +299,20 @@ def fit_3D_curve():
     # print([np.max(spacings[0]), np.max(spacings[1])])
 
     # Peform grad descent
-    num_iter = 50
+    num_iter = 5
     opt = torch.optim.LBFGS(iter([control_pts]), lr=0.4, max_iter=3)
 
-    for j in range(num_iter):#pbar:
+    
+    for j in tqdm(range(num_iter)):#pbar:
         def closure():
             nonlocal initial_curve
+            nonlocal init_curve_l
+            nonlocal init_curve_r
             nonlocal final_curve
+            nonlocal final_curve_l
+            nonlocal final_curve_r
+            nonlocal final_ctrl_l
+            nonlocal final_ctrl_r
             opt.zero_grad()
 
             ctrl_pts = control_pts.permute(1, 0)
@@ -297,43 +331,138 @@ def fit_3D_curve():
                 else:
                     final_curve = curve
 
-            #y, x, z 
+            #x, y, z 
             curve = torch.cat((curve, torch.ones((1, curve.size(1)))), dim=0)
             #x, y, z
-            proj1 = torch.matmul(torch.tensor(P1, dtype=torch.float32), curve.permute(1, 0).unsqueeze(-1))
+            proj1 = torch.matmul(P1, curve.permute(1, 0).unsqueeze(-1))
             for i in range(proj1.size(0)):
                 proj1[i] /= proj1[i, 2, 0].clone()
-            proj2 = torch.matmul(torch.tensor(P2, dtype=torch.float32), curve.permute(1, 0).unsqueeze(-1))
+            proj2 = torch.matmul(P2, curve.permute(1, 0).unsqueeze(-1))
             for i in range(proj2.size(0)):
                 proj2[i] /= proj2[i, 2, 0].clone()
             
+            # with torch.no_grad():
+            #     fig = plt.figure(figsize=(4, 6))
+            #     ax1 = fig.add_subplot(3, 2, (1, 4), projection="3d")
+            #     ax1.view_init(0, 0)
+            #     ax1.plot(curve[1], curve[0], curve[2])
+
+            #     ax2 = fig.add_subplot(325)
+            #     ax2.imshow(img_l, cmap="gray")
+            #     ax2.plot(proj1[:, 0, 0], proj1[:, 1, 0])
+
+            #     ax3 = fig.add_subplot(326)
+            #     ax3.imshow(img_r, cmap="gray")
+            #     ax3.plot(proj2[:, 0, 0], proj2[:, 1, 0])
+            #     plt.show()
+            #     exit(0)
+            proj1 = proj1[:, :2, 0]
+            proj2 = proj2[:, :2, 0]
+
             with torch.no_grad():
-                plt.figure(1)
-                ax1 = plt.axes(projection="3d")
-                ax1.view_init(0, 0)
-                ax1.plot(curve[1], curve[0], curve[2])
+                if TESTING and init_curve_l == None:
+                    init_curve_l = proj1.clone()
+                    init_curve_r = proj2.clone()
+                else:
+                    final_curve_l = proj1
+                    final_curve_r = proj2
 
-                plt.figure(2)
-                ax2 = plt.axes()
-                ax2.plot(proj1[:, 0, 0], proj1[:, 1, 0])
+                    aug_ctrl_pts = torch.cat(
+                        (control_pts, torch.ones((1, control_pts.size(1)))), dim=0
+                    ).permute(1, 0).unsqueeze(-1)
+                    #x, y, z
+                    final_ctrl_l = torch.matmul(P1, aug_ctrl_pts)
+                    for i in range(final_ctrl_l.size(0)):
+                        final_ctrl_l[i] /= final_ctrl_l[i, 2, 0].clone()
+                    final_ctrl_r = torch.matmul(P2, aug_ctrl_pts)
+                    for i in range(final_ctrl_r.size(0)):
+                        final_ctrl_r[i] /= final_ctrl_r[i, 2, 0].clone()
 
-                plt.figure(3)
-                ax3 = plt.axes()
-                ax3.plot(proj2[:, 0, 0], proj2[:, 1, 0])
-                plt.show()
-                exit(0)
+
+
+            # get dists for left projection
+            dists_l = torch.zeros(eval_2D)
+            for i, pix1 in enumerate(target1):
+                # bound a sliding window of size 4
+                min_idx = max(0, i*4 - 5)
+                max_idx = min(eval_3D, i*4 + 6) - 3
+
+                min_dist = None
+                for idx in range(min_idx, max_idx):
+                    curr_dist = 0
+                    for off in range(4):
+                        curr_dist += torch.linalg.norm(pix1 - proj1[idx+off])
+                    curr_dist /= 4
+
+                    if min_dist == None:
+                        min_dist = curr_dist
+                    else:
+                        min_dist = torch.min(min_dist, curr_dist)
+                    
+                dists_l[i] = min_dist
             
-            loss = ((target-curve)**2).mean()
+            # get dists for right projection
+            dists_r = torch.zeros(eval_2D)
+            for i, pix2 in enumerate(target2):
+                # bound a sliding window of size 4
+                min_idx = max(0, i*4 - 5)
+                max_idx = min(eval_3D, i*4 + 6) - 3
+
+                min_dist = None
+                for idx in range(min_idx, max_idx):
+                    curr_dist = 0
+                    for off in range(4):
+                        curr_dist += torch.linalg.norm(pix2 - proj2[idx+off])
+                    curr_dist /= 4
+
+                    if min_dist == None:
+                        min_dist = curr_dist
+                    else:
+                        min_dist = torch.min(min_dist, curr_dist)
+                    
+                dists_r[i] = min_dist
+
+            loss = dists_l.mean() + dists_r.mean()
+
+            
             loss.backward(retain_graph=True)
             return loss
 
-        closure()
-        return
-        if (j % 100) < learn_partition:
-            loss = opt_1.step(closure)
-        else:
-            # loss = opt_2.step(closure)
-            pass
+        loss = opt.step(closure)
+
+    if TESTING:
+        with torch.no_grad():
+            fig = plt.figure(figsize=(10, 7))
+            fig.clf()
+            pre = fig.add_subplot(221, projection="3d")
+            pre_l = fig.add_subplot(245)
+            pre_r = fig.add_subplot(246)
+            pre_l.imshow(img_l, cmap="gray")
+            pre_r.imshow(img_r, cmap="gray")
+            pre.set_title("Initial spline")
+            
+            post = fig.add_subplot(222, projection="3d")
+            post_l = fig.add_subplot(247)
+            post_r = fig.add_subplot(248)
+            post_l.imshow(img_l, cmap="gray")
+            post_r.imshow(img_r, cmap="gray")
+            post.set_title("Final spline")
+
+            pre.scatter(init_control_pts[1], init_control_pts[0], init_control_pts[2], color="r", s=2)
+            pre.plot(initial_curve[1], initial_curve[0], initial_curve[2], color="g")
+            pre_l.scatter(init_ctrl_l[0], init_ctrl_l[1], color="r", s=2)
+            pre_l.plot(init_curve_l[0], init_curve_l[1], color="g")
+            pre_r.scatter(init_ctrl_r[0], init_ctrl_r[1], color="r", s=2)
+            pre_r.plot(init_curve_r[0], init_curve_r[1], color="g")
+
+            post.scatter(control_pts[1], control_pts[0], control_pts[2], color="r", s=2)
+            post.plot(final_curve[1], final_curve[0], final_curve[2], color="g")
+            post_l.scatter(final_ctrl_l[0], final_ctrl_l[1], color="r", s=2)
+            post_l.plot(final_curve_l[0], final_curve_l[1], color="g")
+            post_r.scatter(final_ctrl_r[0], final_ctrl_r[1], color="r", s=2)
+            post_r.plot(final_curve_r[0], final_curve_r[1], color="g")
+
+            plt.show()
     
 
     
