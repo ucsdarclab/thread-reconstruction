dc4bb40b446533b3a00f496ceb83080a8ff79af0
Neelay Joglekar <njogleka@ucsd.edu>
2022-07-19 15:14:23 -0700

    cleanup

--- src/curve_fit2.py
+++ src/curve_fit2.py
@@ -72,72 +72,6 @@ def curve_fit2(img1, img2):
     knots = np.concatenate((knots_start, knots, knots_end))
     t_star = np.array([np.sum(knots[k+1:k+d+1])/d for k in range(num_bases)]) #Greville abscissae
 
-    # TODO Remove unneccessary commented code
-    "Function continues if you scroll down VV"
-    # b = np.array([0, 2, 2, 0, 0, 1, 1])
-    # tck = interp.BSpline(knots, b, d)
-    # x = np.linspace(start, end, 50)
-    # x_b = t_star#np.linspace(start, end, num_bases)
-    # spline = interp.splev(x, tck)
-    # spline_b = interp.splev(x_b, tck)
-    # e_top = np.where(b>spline_b, b, spline_b)
-    # e_low = np.where(b<spline_b, b, spline_b)
-    # e_low, e_top = get_envelope(tck, t_star)
-    # plt.plot(x, spline)
-    # plt.plot(x_b, e_top, c="r")
-    # plt.plot(x_b, e_low, c="r")
-    # plt.scatter(x_b, b, c="g")
-    # plt.show()
-
-    # TODO remove
-    "Envelope, but more mathy"
-    # l = np.zeros_like(t_star, dtype=np.int32)
-    # knot_idx = d
-    # for i, pt in enumerate(t_star):
-    #     while knots[knot_idx+1] < pt:
-    #         knot_idx += 1
-    #     l[i] = knot_idx
-    # k_low = l[:-1] - (d - 1)
-    # k_high = l[1:]
-    # I_star = np.array([[i for i in range(l[k]-d+1, l[k])] for k in range(num_bases)])
-    # Bki = np.zeros_like(I_star)
-    # for k in range(num_bases):
-    #     for i in I_star[k]:
-    #         if i > k:
-    #             t = knots[i:k_high[k]+1 + d+1]
-    #             c = np.array([t_star[j] - t_star[i] for j in range(i, k_high[k]+1)])
-    #         else:
-    #             t = knots[k_low[k]:i+1 + d+1]
-    #             c = np.array([t_star[i] - t_star[j] for j in range(k_low[k], i+1)])
-    #         Bki[k, i] = interp.BSpline(t, c, d)
-
-    
-
-    # # Set endpoint equality constraints
-    # # TODO Make sure chosen depth for keypoint is correct
-    # start_d = (depth_bounds[start, 2] + depth_bounds[start, 3]) / 2
-    # # TODO add extra checks to make sure no OOB errors occur
-    # # TODO Choose different numerical derivative calculation?
-    # post_start_d = (depth_bounds[start+1, 2] + depth_bounds[start+1, 3]) / 2
-    # pre_start_d = (depth_bounds[start-1, 2] + depth_bounds[start-1, 3]) / 2
-    # post2_start_d = (depth_bounds[start+2, 2] + depth_bounds[start+1, 3]) / 2
-    # a0 = start_d
-    # a1 = post_start_d - a0
-    # a2 = a1 - (a0 - pre_start_d)
-    # next_2nd = (post2_start_d - post_start_d) - a1
-    # a3 = next_2nd - a2
-    
-    # # TODO As above
-    # end_d = (depth_bounds[end, 2] + depth_bounds[end, 3]) / 2
-    # post_end_d = (depth_bounds[end+1, 2] + depth_bounds[end+1, 3]) / 2
-    # pre_end_d = (depth_bounds[end-1, 2] + depth_bounds[end-1, 3]) / 2
-    # post2_end_d = (depth_bounds[end+2, 2] + depth_bounds[end+1, 3]) / 2
-    # b0 = end_d
-    # b1 = post_end_d - b0
-    # b2 = b1 - (b0 - pre_end_d)
-    # next_2nd = (post2_end_d - post_end_d) - b1
-    # b3 = next_2nd - b2
-
     low_constr = interp.interp1d(np.arange(start, end+1), depth_bounds[start:end+1, 2])
     high_constr = interp.interp1d(np.arange(start, end+1), depth_bounds[start:end+1, 3])
     center = (low_constr(t_star) + high_constr(t_star))/2
@@ -291,24 +225,6 @@ def upper_bound(args, knots, d, t_star, constr, start, end):
 #         return neg_sum
 #     else:
 #         return np.sum(dists)
-        
-
-
-# def a0_constr(tck, start, a0):
-#     spline = interp.splev([start], tck)
-#     return spline - a0
-
-# def a1_constr(tck, start, a1):
-#     spline = interp.splev([start], tck, 1)
-#     return spline - a1
-
-# def a2_constr(tck, start, a2):
-#     spline = interp.splev([start], tck, 2)
-#     return spline - a2
-
-# def a3_constr(tck, start, a3):
-#     spline = interp.splev([start], tck, 3)
-#     return spline - a3
 
     
 
